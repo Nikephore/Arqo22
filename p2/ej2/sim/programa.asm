a81452ce3eae3eaeb1a994573759c2bfdb05bc00
Luis Nucifora <47322414+Nikephore@users.noreply.github.com>
2022-10-27 20:00:59 +0200

    Add files via upload

--- p2/ej2/sim/programa.asm
+++ p2/ej2/sim/programa.asm
@@ -11,157 +11,84 @@
 #
 ############################################################################
 
-.data
-num0:   .word  1 # posic base + 0
-num1:   .word  2 # posic base + 4
-num2:   .word  4 # posic base + 8
-num3:   .word  8 # posic base + 12
-num4:   .word 16 # posic base + 16
-num5:   .word 32 # posic base + 20
-num32:  .word 0xAAAA5555 # posic base + 24
-buffer: .space 4
+.data 0
+num0:  .word 1  # posic 0
+num1:  .word 2  # posic 4
+num2:  .word 4  # posic 8 
+num3:  .word 8  # posic 12 
+num4:  .word 16 # posic 16 
+num5:  .word 32 # posic 20
+num6:  .word 0  # posic 24
+num7:  .word 0  # posic 28
+num8:  .word 0  # posic 32
+num9:  .word 0  # posic 36
+num10: .word 0  # posic 40
+num11: .word 0  # posic 44
 
-.text 
-main: #Pruebas Load Store y Lui
-      lui  t0, %hi(num0)  # Carga la parte alta de la dir num0
-      nop
-      nop
-      nop
-      lw   t1, 0(t0)      # En x6 un 1
-      lw   t2, 4(t0)      # En x7 un 2
-      lw   t3, 8(t0)      # En x28 un 4 
-      lw   t4,12(t0)      # En x29 un 8
-      lw   t5,16(t0)      # En x30 un 16
-      lw   t6,20(t0)      # En x31 un 32
-     
-      sw   t1, 32(t0)     # Graba x6 en dir 32 (un 1)
-      sw   t2, 36(t0)     # Graba x7 en dir 36 (En x6 un 2)
-      sw   t3, 40(t0)     # Graba x28 en dir 40 (En x7 un 4) 
-      sw   t4, 44(t0)     # Graba x29 en dir 44 (En x29 un 8)
-      sw   t5, 48(t0)     # Graba x30 en dir 48 (En x30 un 16)
-      sw   t6, 52(t0)     # Graba x31 en dir 52 (En x31 un 32)
-
-      lw   a1, 32(t0)     # Graba en x11 desde dir 32 (un 1)
-      lw   a2, 36(t0)     # Graba en x12 desde dir 36 (un 2)
-      lw   a3, 40(t0)     # Graba en x13 desde dir 40 (un 4) 
-      lw   a4, 44(t0)     # Graba en x14 desde dir 44 (un 8)
-      lw   a5, 48(t0)     # Graba en x15 desde dir 48 (un 16)
-      lw   a6, 52(t0)     # Graba en x16 desde dir 52 (un 32)
-      
-      bne  t1, a1, error_LW # tienen que ser iguales y no saltar
-      bne  t2, a2, error_LW
-      bne  t3, a3, error_LW
-      bne  t4, a4, error_LW
-      bne  t5, a5, error_LW
-      bne  t6, a6, error_LW
-      
-      # si no ha saltado OK
-Pru_add:
-      li   t1, 55        # En x6 un 55. Asume que addi funciona correctamente
-      li   t2, 77        # En x7 un 77. Asume que addi funciona correctamente
-      add  a0, t2, t1    # En x10, 55+77=132. En Pipeline sin adelantamiento puede fallar
-      add  a1, t2, t1    # En x11, 55+77=132. En Pipeline sin adelantamiento puede fallar   
-      add  a2, t2, t1    # En x12, 55+77=132. En Pipeline sin adelantamiento puede fallar
-      add  a3, t2, t1    # En x13, 55+77=132.        
-      nop
-      nop
-      nop
-Pru_and:
-      li   t3, 0xF0      # En x28 0xF0
-      nop
-      nop
-      nop
-      and  a4, a3, t3    # En x14 0x80
-      andi a5, a3, 0xF0  # En x15 0x80
-      nop
-      nop
-      nop     
-      beq  a4, a5, OK_AND
-      nop
-      nop
-      nop
-      j    error_AND
-      nop
-      nop
-      nop
-OK_AND: 
-Pru_xor: 
-      addi t4, zero, -1  # cargar x29 con FFF..FFF           
-      lw   t3, 24(t0)    # En x28 AAAA5555
-      xor  t5, t3, t4    # t5 = not (t3), es decir 5555AAAA
-      xor  t6, t5, t3    # debe dar -1. t6 = t3 xor not(x3)
-      # Este grupo de instrucciones puede fallar por riesgos de datos.
-      # lo siguiente repite calculo dejando "burbujas" entre instrucciones
-      nop
-      nop
-      xor  t5, t3, t4    # t5 = not (t3), es decir 5555AAAA
-      nop
-      nop
-      nop
-      xor  t6, t5, t3    # debe dar -1. t6 = t3 xor not(x3)   
-
- 
-Pru_AUIPC:    
-      la t0, buffer         # carga la dreccion del buffer en t0 (X5) [auiPC + addi). No funciona sin control de riesgos
-      lui  t1, %hi(buffer)    # Carga la parte alta de la dir buffer
-      nop
-      nop
-      nop
-      addi t1, t1, %lo(buffer) # Carga la parte baja de la dir buffer
-      nop
-      nop
-      nop
-      # bne t0, t1, error_AUI    # X5 y X6 deben ser iguales. Descomentar con control de riesgos.
-      nop
-      nop
-      nop
-           
-Pru_jal:  
-      jal ra, salto_y_regreso           
-      nop
-      nop
-      nop
-      addi t1, zero, -15
-      nop
-      nop
-      nop
-      bne t0, t1, error_JAL
-      j OK_prog
-           
-salto_y_regreso: # en ra(x1) tiene que estar la dir de retorno
-      addi t0, zero -15  # guardo en x5 valor FFFF_FFF1. Como marca
-      nop
-      nop
-      nop
-      jalr zero, 0(ra)   # retorno al llamado
-      nop
-      nop
-      nop
-      addi t0, zero, 0xDD #nunca debe ejecutarse
-
-#--- Buclwa errores y final de prog 
-error_LW:  addi t1, t1, -1      # decrementa infinitamente t0
-           beq  x0, x0, error_LW
-           nop
-           nop
-           nop
-error_AND: addi t2, t2, -1      # decrementa infinitamente t0
-           beq  x0, x0, error_AND
-           nop
-           nop
-           nop
-error_AUI: addi t3, t3, -1      # decrementa infinitamente t0
-           beq  x0, x0, error_AUI
-           nop
-           nop
-           nop    
-error_JAL: addi t4, t4, -1      # decrementa infinitamente t4
-           beq  x0, x0, error_JAL
-           nop
-           nop
-           nop       
-OK_prog:   addi t6, t6, 1      # incrementa infinitamente t6 (x31)
-           beq  x0, x0, OK_prog
-           nop
-           nop
-           nop
+.text 0
+main:
+  # carga num0 a num5 en los registros 9 a 14
+  lw $t1, 0($zero)  # lw $r9,  0($r0)  -> r9  = 1
+  lw $t2, 4($zero)  # lw $r10, 4($r0)  -> r10 = 2
+  lw $t3, 8($zero)  # lw $r11, 8($r0)  -> r11 = 4 
+  lw $t4, 12($zero) # lw $r12, 12($r0) -> r12 = 8 
+  lw $t5, 16($zero) # lw $r13, 16($r0) -> r13 = 16
+  lw $t6, 20($zero) # lw $r14, 20($r0) -> r14 = 32
+  # copia num0 a num5 sobre num6 a num11
+  sw $t1, 24($zero) # sw $r9,  24($r0) -> data[24] =  1
+  sw $t2, 28($zero) # sw $r10, 28($r0) -> data[28] =  2
+  sw $t3, 32($zero) # sw $r11, 32($r0) -> data[32] =  4 
+  sw $t4, 36($zero) # sw $r12, 36($r0) -> data[36] =  8 
+  sw $t5, 40($zero) # sw $r13, 40($r0) -> data[40] =  16
+  sw $t6, 44($zero) # sw $r14, 44($r0) -> data[44] =  32
+  # carga num6 a num11 en los registros 9 a 14, deberia ser lo mismo
+  lw $t1, 24($zero) # lw $r9, 24($r0)  -> r9  no cambia
+  lw $t2, 28($zero) # lw $r10, 28($r0) -> r10 no cambia
+  lw $t3, 32($zero) # lw $r11, 32($r0) -> r11 no cambia
+  lw $t4, 36($zero) # lw $r12, 36($r0) -> r12 no cambia
+  lw $t5, 40($zero) # lw $r13, 40($r0) -> r13 no cambia
+  lw $t6, 44($zero) # lw $r14, 44($r0) -> r14 no cambia
+  
+  # realiza operaciones
+  add $t7, $t1, $t2 # add $r15, $r9, $r10  -> r15=r9+r10      = 3
+  add $s0, $t3, $t4 # add $r16, $r11, $r12 -> r16=r11+r12     = 12
+  xor $s1, $t1, $t2 # xor $r17, $r9, $r10  -> r17=r9 xor r10  = 3
+  xor $s2, $t7, $t2 # xor $r18, $r15, $r10 -> r18=r15 xor r10 = ERROR del micro: deberia ser 1, pero por dependencia sale 2
+  sub $s2, $t3, $t1 # sub $r18, $r11, $r9  -> r18=r11-r9      = 3
+  and $s3, $t1, $t2 # and $r19, $r9, $r10  -> r19=r9 and r10  = 0, no cambia
+  and $s4, $t7, $t2 # and $r20, $r15, $r10 -> r20=r15 and r10 = 2
+  or $s5, $t1, $t2  # or $r21, $r9, $r10   -> r21=r9 or r10   = 3
+  or $s6, $s0, $t2  # or $r22, $r16, $r10  -> r22=r16 or r10  = 14
+  slt $s7, $t1, $t2 # slt $r23, $r9, $r10  -> r23=1 si r9 < r10 else 0  = 1
+  slt $t8, $s0, $t2 # slt $r24, $r16, $r10 -> r24=1 si r16 < r10 else 0 = 0
+  
+  # carga datos inmediatos en la parte alta de registros
+  lui $t1, 1 # lui $r9, 1  -> queda a 65536  (0x00010000)
+  lui $t2, 2 # lui $r10, 2 -> queda a 131072 (0x00020000)
+  
+  # salto condicional: solo comprobamos por ahora que salte o no y la direccion 
+  # de salto, pero el procesador ejecutara incorrectamente varias de las 
+  # instrucciones tras el beq:
+  #     (nota: tras cada beq el ensamblador insertara un NOP -instruccion 0x000000000-
+  #     debido al "delayed branching" de MIPS)
+  beq $s1, $s2, salto # beq $r17, $r18, salto -> como $r17=$r18=3, salta
+  add $t0, $s0, $s1   # add $r8,  $r16, $r17  -> se ejecuta, r8  = 15
+  add $t1, $s2, $s3   # add $r9,  $r18, $r19  -> se ejecuta, r9  = 3
+  add $t2, $s4, $s5   # add $r10, $r20, $r21  -> no se ejecuta, r10 no cambia
+salto:
+  sub $t4, $s0, $t7     # sub $r12, $r16, $r15    -> r12 = 9
+  beq $s3, $s4, nosalto # beq $r19, $r20, nosalto -> este branch NO debe ejecutarse
+final:
+  beq $zero, $zero, final # -> bucle infinito, volvera aqui tras varios NOPs.
+  nop
+  nop
+  nop
+  nop
+nosalto:
+  lui $t2, 0xFFFF  # lui $r10, 0xFFFF -> no debe ejecutarse
+  beq $zero, $zero, final
+  nop
+  nop
+  nop
+  nop
+  
