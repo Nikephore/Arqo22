95c99e5ccb5f25e2559b06d5be08cf72ac5afd69
BernardoZF <44814561+BernardoZF@users.noreply.github.com>
2022-10-28 21:40:39 +0200

    Update programa.asm

--- p2/ej1/sim/programa.asm
+++ p2/ej1/sim/programa.asm
@@ -42,74 +42,30 @@ main:
   # RIESGOS REGISTRO REGISTRO
   add t3, t1, t2 # en r11 un 3 = 1 + 2
   add t1, t3, t2 # dependencia con la anterior # en r9 un 5 = 2 + 3
-  nop
-  nop
-  nop
   add t3, t1, t2 # en r11 un 7 = 5 + 2
-  nop
   add t2, t4, t3 #dependencia con la 2� anterior # en r10 un 15 = 7 + 8
-  nop
-  nop
-  nop
   add t3, t1, t2  # en r11 un 20 = 5 + 15
-  nop
-  nop
   add t2, t3, t5 #dependencia con la 3� anterior  # en r10 un 36 = 20 + 16
-  nop
-  nop
-  nop
   add s0, t1, t2  # en r16 un 41 = 5 + 36
   add s0, s0, s0  # Dependencia con la anterior  # en r16 un 82 = 41 + 41. 
   add s1, s0, s0  # dependencia con la anterior  # en r16 un 164 = 82 + 82
-  nop
-  nop
-  nop
-  nop
   # RIESGOS REGISTRO MEMORIA
   add t3, t1, t2 # en r11 un 41 = 5 + 36
   sw t3, 24(zero) # dependencia con la anterior
-  nop
-  nop
-  nop
   add t4, t1, t2 # en r12 un 41 = 5 + 36
-  nop
   sw t4, 28(zero) # dependencia con la 2� anterior
-  nop
-  nop
-  nop
   add t5, t1, t2 # en r13 un 41 = 5 + 36
-  nop
-  nop
   sw t5, 32(zero) # dependencia con la 3� anterior
-  nop
-  nop
-  nop
-  nop
   # RIESGOS MEMORIA REGISTRO
   lw t3, 0(zero) # en r11 un 1
   add t4, t2, t3 # dependencia con la anterior # en r12 37 = 36 + 1
-  nop
-  nop
-  nop
   lw t3, 4(zero) # en r11 un 2
-  nop
   add t4, t2, t3 # dependencia con la 2� anterior # en r12 38 = 36 + 2
-  nop
-  nop
   lw t3, 8(zero) # en r11 un 4
-  nop
-  nop
   add t4, t2, t3 # dependencia con la 3� anterior # en r12 40 = 36 + 4
-  nop
-  nop
-  nop
   # RIESGOS MEMORIA MEMORIA
   sw t4, 0(zero)
   lw t2, 0(zero) # en r10 un 40
-  nop
-  nop
-  nop
-  nop
   lw t2, 4(zero) # en r10 un 2
   sw t2, 0(zero) # Guarda el 2 en posicion 0 de memoria
   # syscall
