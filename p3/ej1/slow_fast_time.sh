c298162e37201e727e9fa2d959ed6accf449b3e9
bernardo <bernarrr69@gmail.com>
2022-11-14 09:04:04 +0100

    P3 acabado falta hacer la memoria

---
+++ p3/ej1/slow_fast_time.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+#$ -S /bin/bash
+#$ -cwd
+#$ -o salida.out
+#$ -j y
+
+# Anadir valgrind y gnuplot al path
+export PATH=$PATH:/share/apps/tools/valgrind/bin:/share/apps/tools/gnuplot/bin
+# Indicar ruta librerías valgrind
+export VALGRIND_LIB=/share/apps/tools/valgrind/lib/valgrind
+
+# inicializar variables
+Ninicio=$((1024))
+Npaso=1024
+Nfinal=$((16384))
+fDAT=slow_fast_time.dat
+fPNG=slow_fast_time.png
+Iter=13
+
+
+# generar el fichero DAT vacío
+touch $fDAT
+
+echo "Running slow and fast..."
+#for N in $(seq $Ninicio $Npaso $Nfinal);
+for i in $(seq 1 1 $Iter); do
+	echo "Iteracion: $i"
+	for ((j=0, N=Ninicio ; N<=Nfinal ; N+=Npaso, j+=1)); do
+		if [ "$i" = "0" ]; then
+			slowTime_t[$j]=0
+			fastTime_t[$j]=0
+		fi
+
+		echo "N: $N / $Nfinal..."
+		# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
+		# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
+		# tercera columna (el valor del tiempo). Dejar los valores en variables
+		# para poder imprimirlos en la misma línea del fichero de datos
+		slowTime=$(./slow "$N" | grep 'time' | awk '{print $3}')
+		fastTime=$(./fast "$N" | grep 'time' | awk '{print $3}')
+
+		slowTime_t[$j]=$(echo "${slowTime_t[$j]}" "$slowTime" | awk '{print $1 + $2}')
+		fastTime_t[$j]=$(echo "${fastTime_t[$j]}" "$fastTime" | awk '{print $1 + $2}')
+	done
+done
+
+echo "Computing the average..."
+for ((j = 0, N = Ninicio ; N <= Nfinal ; N += Npaso, j += 1)); do
+	echo "N: $N / $Nfinal..."
+
+	slowTime=$(echo "${slowTime_t[$j]}" "$Iter" | awk '{print $1 / $2}')
+	fastTime=$(echo "${fastTime_t[$j]}" "$Iter" | awk '{print $1 / $2}')
+
+	echo "$N	$slowTime	$fastTime" >> $fDAT
+done
+
+echo "Generating plot..."
+gnuplot << END_GNUPLOT
+set title "Slow-Fast Execution Time"
+set ylabel "Execution time (s)"
+set xlabel "Matrix Size"
+set key right bottom
+set grid
+set term png
+set output "$fPNG"
+plot "$fDAT" using 1:2 with lines lw 2 title "slow", \
+     "$fDAT" using 1:3 with lines lw 2 title "fast"
+replot
+quit
+END_GNUPLOT
---
+++ p3/ej2/slow_fast_time.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+#$ -S /bin/bash
+#$ -cwd
+#$ -o salida.out
+
+#$ -j y
+# Anadir valgrind y gnuplot al path
+export PATH=$PATH:/share/apps/tools/valgrind/bin:/share/apps/tools/gnuplot/bin
+# Indicar ruta librerías valgrind
+export VALGRIND_LIB=/share/apps/tools/valgrind/lib/valgrind
+
+# inicializar variables
+Ninicio=$((1024 + 1024 * 13))
+Npaso=256
+Nfinal=$((1024 + 1024 * 14))
+tamanios=(1024 2048 4096 8192)
+
+rm -f ./*.dat ./*.png
+
+echo "Running valgrind slow and fast..."
+for ((N=Ninicio; N<=Nfinal; N+=Npaso)); do
+    for i in "${tamanios[@]}"; do
+        valgrind --tool=cachegrind --cachegrind-out-file=slow.out --I1="$i",1,64 --D1="$i",1,64 --LL=8388608,1,64 ./slow "$N"
+        valgrind --tool=cachegrind --cachegrind-out-file=fast.out --I1="$i",1,64 --D1="$i",1,64 --LL=8388608,1,64 ./fast "$N"
+
+        slowD1mr=$(cg_annotate slow.out | grep PROGRAM | awk '{print $5}' | tr -d ',')
+        slowD1mw=$(cg_annotate slow.out | grep PROGRAM | awk '{print $8}' | tr -d ',')
+        fastD1mr=$(cg_annotate fast.out | grep PROGRAM | awk '{print $5}' | tr -d ',')
+        fastD1rw=$(cg_annotate fast.out | grep PROGRAM | awk '{print $8}' | tr -d ',')
+
+        echo "$N $slowD1mr $slowD1mw $fastD1mr $fastD1rw" >> cache_"$i".dat
+    done
+done
+
+rm ./*.out
+
+echo "Generating plot..."
+# llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
+# estándar el script que está entre "<< END_GNUPLOT" y "END_GNUPLOT"
+gnuplot << END_GNUPLOT
+set title "Cache Data Read Errors"
+set ylabel "Number of Errors"
+set xlabel "Matrix Size"
+set key right bottom
+set grid
+set term png
+set output "cache_lectura.png"
+plot "cache_1024.dat" using 1:2 with lines lw 2 title "slow 1024", \
+    "cache_1024.dat" using 1:4 with lines lw 2 title "fast 1024", \
+    "cache_2048.dat" using 1:2 with lines lw 2 title "slow 2048", \
+    "cache_2048.dat" using 1:4 with lines lw 2 title "fast 2048", \
+    "cache_4096.dat" using 1:2 with lines lw 2 title "slow 4096", \
+    "cache_4096.dat" using 1:4 with lines lw 2 title "fast 4096", \
+    "cache_8192.dat" using 1:2 with lines lw 2 title "slow 8192", \
+    "cache_8192.dat" using 1:4 with lines lw 2 title "fast 8192"
+replot
+set title "Cache Data Write Errors"
+set output "cache_escritura.png"
+plot "cache_1024.dat" using 1:3 with lines lw 2 title "slow 1024", \
+     "cache_1024.dat" using 1:5 with lines lw 2 title "fast 1024", \
+     "cache_2048.dat" using 1:3 with lines lw 2 title "slow 2048", \
+     "cache_2048.dat" using 1:5 with lines lw 2 title "fast 2048", \
+     "cache_4096.dat" using 1:3 with lines lw 2 title "slow 4096", \
+     "cache_4096.dat" using 1:5 with lines lw 2 title "fast 4096", \
+     "cache_8192.dat" using 1:3 with lines lw 2 title "slow 8192", \
+     "cache_8192.dat" using 1:5 with lines lw 2 title "fast 8192"
+replot
+quit
+END_GNUPLOT
