d6ef0c783aa9ca37577f6028e253edcb6440ffe5
Luis Nucifora <47322414+Nikephore@users.noreply.github.com>
2022-12-10 00:41:19 +0100

    Update ej3_data.sh

--- p4/ejercicio3/ej3_data.sh
+++ p4/ejercicio3/ej3_data.sh
@@ -8,9 +8,10 @@ echo "Running normal_serie and normal_par..."
 
 for((N=Ninicio; N<= Nfinal; N+=Npaso)); do
     echo "Damos una vuelta"
+    echo "Tamanio de matriz: $N"
     normalserieTime=$(../normal_serie "$N" | grep 'time' | awk '{print $3}')
     normalparalelTime=$(../normal_par1 "$N" 4 | grep 'time' | awk '{print $3}')
 
-    echo "$N $normalserieTime $normalparalelTime" >> ej3.dat
+    echo "$N $normalserieTime $normalparalelTime" >> ejercicio3.dat
 done
 
